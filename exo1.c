c794a9eadca86e7886163c4587c618920196f8a4
MK8BK <64875519+MK8BK@users.noreply.github.com>
2022-10-17 17:01:39 +0200

    added ackermann 1, 1.2.9

--- exo1.c
+++ exo1.c
@@ -30,11 +30,43 @@ float power_v3(float x, int n){
 	return r;
 }
 
+void pow_v9(float x, int n, float *r){
+	if (n==0){
+		return;
+	}
+	else{
+		if (n%2!=0){
+			*r=*r * x;
+		}
+		pow_v9(x*x,n/2,r);
+	}
+}
+
+float power_v9(float x, int n){
+	float r=1.0f ;
+	float *ptr= &r;
+	pow_v9(x,n,ptr);
+	return r;
+}
+
+
+long long int Ackermann(int m, int n){
+	if(m==0){
+		return n+1;
+	}else if(m>0 && n==0){
+		return Ackermann(m-1, 1);
+	}else if(m>0 && n>0){
+		return Ackermann(m-1, Ackermann(m, n-1));
+	}
+}
+
 
 int main(){
 	//int M=33;
 	//float E = e(0,M,1,0.0);
-	float xn = power(4.5f, 10);
+	float xn = power_v9(4.5f, 10);
 	printf("%f\n",xn);
+	long long int AAAAAAAAArgh = Ackermann(5,0);
+	printf("%lld",AAAAAAAAArgh);
 	return 0;
 }
