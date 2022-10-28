cdd3524040b45b3798a9ce23542d0032d9f160d0
MK8BK <64875519+MK8BK@users.noreply.github.com>
2022-10-28 15:42:33 +0200

    duh

--- listes-piles-bruh.c
+++ listes-piles-bruh.c
@@ -23,8 +23,8 @@ typedef enum { FALSE, TRUE} bool;
 
 typedef struct Bloc
 {
-    int nombre;
-    struct Bloc *suivant;
+	int nombre;
+	struct Bloc *suivant;
 } Bloc;
 
 typedef Bloc *Liste ;
@@ -80,43 +80,43 @@ void VireDernier_iter (Liste *L);
 
 void initVide( Liste *L)
 {
-    *L = NULL ;
+	*L = NULL ;
 }
 
 bool estVide(Liste l)
 {
-    return l == NULL ;
+	return l == NULL ;
 }
 
 int premier(Liste l)
 {
-    return l->nombre ; 
+	return l->nombre ; 
 }
 
 Liste ajoute(int x, Liste l)
 {
-    Liste tmp = (Liste) malloc(sizeof(Bloc)) ;
-    tmp->nombre = x ;
-    tmp->suivant = l ;
-    return tmp ;
+	Liste tmp = (Liste) malloc(sizeof(Bloc)) ;
+	tmp->nombre = x ;
+	tmp->suivant = l ;
+	return tmp ;
 }
 
 void empile(int x, Liste *L)
 {
-      *L = ajoute(x,*L) ; 
+	  *L = ajoute(x,*L) ; 
 }
 
 Liste suite(Liste l)
 {
-    return l->suivant ;
+	return l->suivant ;
 }
 
 
 void depile(Liste *L)
 {
-    Liste tmp = *L ;
-    *L = suite(*L) ;
-    free(tmp) ;
+	Liste tmp = *L ;
+	*L = suite(*L) ;
+	free(tmp) ;
 }
 
 /*************************************************/
@@ -127,25 +127,25 @@ void depile(Liste *L)
 
 void affiche_rec(Liste l)
 {
-    if(estVide(l))
-        printf("\n");
-    else
-    {
-        printf("%d ", premier(l));
-        affiche_rec(suite(l));
-    }
+	if(estVide(l))
+		printf("\n");
+	else
+	{
+		printf("%d ", premier(l));
+		affiche_rec(suite(l));
+	}
 }
 
 
 void affiche_iter(Liste l)
 {
-    Liste L2 = l;
-    while(!estVide(L2))
-    {
-        printf("%d ", premier(L2));
-        L2 = suite(L2);
-    }
-    printf("\n");
+	Liste L2 = l;
+	while(!estVide(L2))
+	{
+		printf("%d ", premier(L2));
+		L2 = suite(L2);
+	}
+	printf("\n");
 }
 
 /*************************************************/
@@ -156,21 +156,21 @@ void affiche_iter(Liste l)
 
 int longueur_rec (Liste l)
 {
-    if (l == NULL)
-         return 0 ;
-    else return (1 + longueur_rec(l->suivant)) ;
+	if (l == NULL)
+		 return 0 ;
+	else return (1 + longueur_rec(l->suivant)) ;
 }
 
 
 int longueur_iter (Liste l)
 {
-    Liste P = l;
-    int cpt = 0 ;
-    while (P ISNOT NULL)
-    {   P = P->suivant ;
-        cpt++ ;
-    }
-    return cpt ;
+	Liste P = l;
+	int cpt = 0 ;
+	while (P ISNOT NULL)
+	{   P = P->suivant ;
+		cpt++ ;
+	}
+	return cpt ;
 }
 
 /*************************************************/
@@ -182,28 +182,28 @@ int longueur_iter (Liste l)
 /*************************************************/
 
 void VD (Liste *L)
-          // *L non NULL ie liste non vide
+		  // *L non NULL ie liste non vide
 {
-     if ( ((**L).suivant) == NULL )
-            depile(L) ;   // moralement : depile(& (*L)) ;
-     else VD (& ( (**L).suivant )) ;
+	 if ( ((**L).suivant) == NULL )
+			depile(L) ;   // moralement : depile(& (*L)) ;
+	 else VD (& ( (**L).suivant )) ;
 }
 
 void VireDernier_rec (Liste *L)
 {
-     if ( (*L) ISNOT NULL )
-          VD(L);        // moralement : VD(& (*L)) ;
+	 if ( (*L) ISNOT NULL )
+		  VD(L);        // moralement : VD(& (*L)) ;
 }
 
 void VireDernier_iter (Liste *L)
 {
-    if ( (*L) ISNOT NULL)
-    {
-        while ( ((**L).suivant) ISNOT NULL )
-                 L = & ( (**L).suivant ) ;
-        free(*L) ;
-        *L = NULL ;
-     }
+	if ( (*L) ISNOT NULL)
+	{
+		while ( ((**L).suivant) ISNOT NULL )
+				 L = & ( (**L).suivant ) ;
+		free(*L) ;
+		*L = NULL ;
+	 }
 }
 
 
@@ -215,12 +215,12 @@ void VireDernier_iter (Liste *L)
 
 void VideListe(Liste *L)
 {
-    if(NOT(estVide(*L)))
-    {
-        depile(L);
-        VideListe(L);
-    }
-      
+	if(NOT(estVide(*L)))
+	{
+		depile(L);
+		VideListe(L);
+	}
+	  
 }
 
 /*************************************************/
@@ -231,14 +231,14 @@ void VideListe(Liste *L)
 
 void poup (Liste l)
 {
-        printf("Double Affichage \n") ;
-        affiche_rec(l) ;
-        affiche_iter(l) ;
+		printf("Double Affichage \n") ;
+		affiche_rec(l) ;
+		affiche_iter(l) ;
 
-        printf("Longueur en double %d %d \n\n", 
-                           longueur_rec(l), 
-                           longueur_iter(l) 
-               ) ;
+		printf("Longueur en double %d %d \n\n", 
+						   longueur_rec(l), 
+						   longueur_iter(l) 
+			   ) ;
 }
 
 /*************************************************/
@@ -248,123 +248,145 @@ void poup (Liste l)
 /*************************************************/
 
 int pos_0123(Liste l){
-    if (estVide(l)){
-        return 0;
-    } else if (premier(l)==0){
-        return 1;
-    } else if (NOT (estVide(suite(l))) AND premier(suite(l))==0){
-        return 1;
-    } else if (NOT (estVide(suite(suite(l)))) AND premier(l->suivant->suivant)==0){
-        return 1;
-    }
-    return 0;
+	if (estVide(l)){
+		return 0;
+	} else if (premier(l)==0){
+		return 1;
+	} else if (NOT (estVide(suite(l))) AND premier(suite(l))==0){
+		return 1;
+	} else if (NOT (estVide(suite(suite(l)))) AND premier(l->suivant->suivant)==0){
+		return 1;
+	}
+	return 0;
 }
 
 int pluscourte(Liste l1, Liste l2){
-    while (l1!=NULL AND l2!=NULL){
-        l1=suite(l1);
-        l2=suite(l2);
-    }
-    if (l1==NULL AND l2!=NULL){
-        return 1;
-    }
-    return 0;
+	while (l1!=NULL AND l2!=NULL){
+		l1=suite(l1);
+		l2=suite(l2);
+	}
+	if (l1==NULL AND l2!=NULL){
+		return 1;
+	}
+	return 0;
 }
 
 int nb0k_vi(Liste l1, int k){//iteratif
-    int cpt=0;
-    for(int i=0;i<k;i++){
-        if (premier(l1)==0){
-            cpt+=1;
-        }
-        l1=suite(l1);
-    }
-    return cpt;
+	int cpt=0;
+	for(int i=0;i<k;i++){
+		if (premier(l1)==0){
+			cpt+=1;
+		}
+		l1=suite(l1);
+	}
+	return cpt;
 }
 
 int nb0k_vr(Liste l1, int k){//recursif simple
-    if (k==1 && premier(l1)==0){
-        return 1;
-    }
-    if (k==1 && premier(l1)!=0){
-        return 0;
-    }
-    if (premier(l1)!=0) {
-        return nb0k_vr(suite(l1),k-1);
-    }
-    return 1+nb0k_vr(suite(l1),k-1);
+	if (k==1 && premier(l1)==0){
+		return 1;
+	}
+	if (k==1 && premier(l1)!=0){
+		return 0;
+	}
+	if (premier(l1)!=0) {
+		return nb0k_vr(suite(l1),k-1);
+	}
+	return 1+nb0k_vr(suite(l1),k-1);
 
 }
 int s_f(Liste l1, int k , int acc){ //fonction pour recursif terminale
-    if (k==1 && premier(l1)==0){
-        return acc+1;
-    }
-    if (k==1 && premier(l1)!=0){
-        return acc;
-    }
-    if (premier(l1)!=0) {
-        return s_f(suite(l1),k-1,acc);
-    }
-    return s_f(suite(l1),k-1,acc+1);
+	if (k==1 && premier(l1)==0){
+		return acc+1;
+	}
+	if (k==1 && premier(l1)!=0){
+		return acc;
+	}
+	if (premier(l1)!=0) {
+		return s_f(suite(l1),k-1,acc);
+	}
+	return s_f(suite(l1),k-1,acc+1);
 }
+
 int nb0k_vrtf(Liste l1, int k){//recursif terminale
-    return s_f(l1,k,0);
+	return s_f(l1,k,0);
 }
+
+void s_p(Liste l1, int k, int *acc){
+	if (k==1 && premier(l1)==0){
+		*acc+=1;
+	}
+	else if (premier(l1)!=0) {
+		s_p(suite(l1),k-1,acc);
+	}
+	else{
+		*acc+=1;
+		s_p(suite(l1),k-1,acc);
+	}
+}
+
+
+int nb0k_vrtp(Liste l1, int k){//recursif terminale sous_procedure
+	int acc=0;
+	int *p_acc=&acc;
+	s_p(l1,k,p_acc);
+	return acc;
+}
+
+Liste Fctbegaye(Liste l1){
+	if (estVide(l1)){
+		return l1;
+	}
+	if (premier(l1)>0){
+		return ajoute(premier(l1),ajoute(premier(l1),Fctbegaye(suite(l1))));
+	}
+	else{
+		return ajoute(premier(l1),Fctbegaye(suite(l1)));
+	}
+
+}
+
 
 int main(int argc, char** argv)
 {
-    Liste l ;
+	Liste l ;
+	initVide (&l) ;
+	//poup(l) ;
+	empile(0, &l) ;
+	//poup(l) ;
+	empile(5, &l) ;
+	empile(0, &l) ;
+	empile(7, &l) ;
+	empile(8, &l) ;
+	empile(9, &l) ;
+	//poup(l) ;
+	VireDernier_rec  (&l) ;
+	VireDernier_iter (&l) ;
+	//depile(& l) ;
+	//poup(l) ;
+	Liste l2 ;
+	initVide (&l2) ;
+	empile(0, &l) ;
+	//poup(l) ;
+	empile(0, &l2) ;
+	empile(1, &l2) ;
+	empile(0, &l2) ;
+	empile(0, &l2) ;
+	empile(0, &l2) ;
+	empile(9, &l2) ;
+	empile(6, &l2) ;
+	empile(5, &l2) ;
+	empile(8, &l2) ;
+	empile(2, &l2) ;
+	//VideListe(&l);
 
-        initVide (&l) ;
-
-          //poup(l) ;
-
-             empile(0, &l) ;
-
-          //poup(l) ;
-
-             empile(5, &l) ;
-             empile(0, &l) ;
-             empile(7, &l) ;
-             empile(8, &l) ;
-             empile(9, &l) ;
-
-          //poup(l) ;
-
-        VireDernier_rec  (&l) ;
-        VireDernier_iter (&l) ;
-        //depile(& l) ;
-
-          //poup(l) ;
-          
-     Liste l2 ;
-
-        initVide (&l2) ;
-        empile(0, &l) ;
-
-          //poup(l) ;
-
-             empile(0, &l2) ;
-             empile(1, &l2) ;
-             empile(0, &l2) ;
-             empile(0, &l2) ;
-             empile(0, &l2) ;
-             empile(9, &l2) ;
-             empile(6, &l2) ;
-             empile(5, &l2) ;
-             empile(8, &l2) ;
-             empile(2, &l2) ;
-             
-
-    //VideListe(&l);
-
-    //Nos tests de fonctions ci-dessous
-    //affiche_rec(l);
-    affiche_rec(l2);
-    //printf("%d\n",pos_0123(l));
-    //printf("%d\n",pluscourte(l,l2));
-    printf("%d\n",nb0k_vrtf(l2,7));
-    return 0;
+	//Nos tests de fonctions ci-dessous
+	//affiche_rec(l);
+	affiche_rec(l2);
+	//printf("%d\n",pos_0123(l));
+	//printf("%d\n",pluscourte(l,l2));
+	printf("%d\n",nb0k_vrtf(l2,7));
+	return 0;
 }
 
 
